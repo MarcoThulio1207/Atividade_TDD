bfb01e4b3d29b24bbfdafe281a49151ec0e6ceae
MarcoThulio1207 <marcothulio120799@gmail.com>
2025-05-19 22:15:53 -0300

    Código refatorado

--- teste-junit/src/test/java/com/exemplo/FatorialTest.java
+++ teste-junit/src/test/java/com/exemplo/FatorialTest.java
@@ -7,8 +7,18 @@ import org.junit.Test;
 public class FatorialTest {
 
     @Test
-    public void testFatorialZero() {
-        assertEquals(2, Fatorial.calcularFatorial(2));
+    public void testBase() {
+        assertEquals(1, Fatorial.calcular(0));
+        assertEquals(1, Fatorial.calcular(1));
     }
 
+    @Test
+    public void testCinco() {
+        assertEquals(120, Fatorial.calcular(5));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativo() {
+        Fatorial.calcular(-1);
+    }
 }
